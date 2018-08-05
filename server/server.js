8c4ab106d1b034ec0f79b244dca0999e7b7370a2
Robin Jones <jonesyr@gmail.com>
2018-08-05 19:10:53 +0200

    Broadcast event

--- server/server.js
+++ server/server.js
@@ -16,14 +16,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'jonesyr@gmail.com',
-    text: 'Supppp',
-    createdAt: 12345
-  });
-
-  socket.on('createMessage', (newMessage) => {
-    console.log('newMessage', newMessage)
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message)
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect', () => {
